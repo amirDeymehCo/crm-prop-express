8c8ad961711c1e645cd03fde2943f7cd03fa4bd7
amir mohammad deymeh 2 <104575719+amirDeymehCo@users.noreply.github.com>
2026-01-05 10:40:29 +0330

    add env config

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-require("dotenv").config();
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`
+});
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
