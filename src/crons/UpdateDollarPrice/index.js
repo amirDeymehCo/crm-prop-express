2ca52ae5231511654ad50d3747eddf4d33e642fe
amir mohammad deymeh 2 <104575719+amirDeymehCo@users.noreply.github.com>
2026-02-15 13:44:50 +0330

    fix cron update price dollar

--- src/crons/UpdateDollarPrice/index.js
+++ src/crons/UpdateDollarPrice/index.js
@@ -3,30 +3,29 @@ const axios = require("axios");
 const Setting = require("../../models/Setting");
 
 async function updatePrice() {
-    try {
-        const { data } = await axios.get("https://api.tetherland.com/currencies", {
-            timeout: 5000,
-        });
+  try {
+    const { data } = await axios.get("https://api.tetherland.com/currencies", {
+      timeout: 5000,
+    });
 
-        const usd = data?.data?.currencies?.USDT;
-        if (!usd) throw new Error("USD not found");
+    const usd = data?.data?.currencies?.USDT;
+    if (!usd) throw new Error("USD not found");
 
-        await Setting.upsert({
-            id: 1,
-            dollar_price: usd.price,
-        });
+    await Setting.upsert({
+      id: 1,
+      dollar_price: usd.price,
+    });
 
-        console.log("Dollar price updated:", usd.price);
-
-    } catch (error) {
-        console.error("Error updating dollar price:", error.message);
-    }
+    console.log("Dollar price updated:", usd.price);
+  } catch (error) {
+    console.error("Error updating dollar price:", error.message);
+  }
 }
 
-// اجرای کرون‌جاب هر 1 دقیقه
-cron.schedule("* * * * *", () => {
-    console.log("Running USD price updater...");
-    updatePrice();
+// اجرای کرون‌جاب هر 12 ساعت
+cron.schedule("0 */12 * * *", () => {
+  console.log("Running USD price updater...");
+  updatePrice();
 });
 
 module.exports = updatePrice;
