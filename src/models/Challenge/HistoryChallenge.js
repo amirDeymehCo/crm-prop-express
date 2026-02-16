9126169849ab6ae7f7387a5bbfe1bebb31b6b708
amir mohammad deymeh 2 <104575719+amirDeymehCo@users.noreply.github.com>
2026-02-16 15:26:06 +0330

    fix challenge_rejected type

--- src/models/Challenge/HistoryChallenge.js
+++ src/models/Challenge/HistoryChallenge.js
@@ -5,23 +5,30 @@ const Admin = require("../Admin");
 const ChallengePlan = require("./ChallengePlan");
 const UserChallenge = require("./UserChallenge");
 
-const HistoryChallenge = sequelize.define("HistoryChallenge", {
+const HistoryChallenge = sequelize.define(
+  "HistoryChallenge",
+  {
     title: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     type: {
-        type: DataTypes.ENUM("change_status", "change_risk")
-    }
-}, {
+      type: DataTypes.ENUM(
+        "change_status",
+        "change_risk",
+        "challenge_rejected",
+      ),
+    },
+  },
+  {
     tableName: "history_challenge",
-    underscored: true
-});
-
+    underscored: true,
+  },
+);
 
 HistoryChallenge.belongsTo(Admin, {
-    foreignKey: "admin_id"
-})
+  foreignKey: "admin_id",
+});
 UserChallenge.hasMany(HistoryChallenge, { foreignKey: "user_challenge_id" });
 HistoryChallenge.belongsTo(UserChallenge, { foreignKey: "user_challenge_id" });
 
