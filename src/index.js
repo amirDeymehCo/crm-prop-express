51437b3f09339f2d9eaa2ed59f6827cde292b1b9
amir mohammad deymeh 2 <104575719+amirDeymehCo@users.noreply.github.com>
2026-02-24 14:17:01 +0330

    back

--- src/index.js
+++ src/index.js
@@ -19,29 +19,30 @@ app.set("trust proxy", 1);
 app.use(cors());
 app.options("*", cors());
 
+app.use(cookieParser());
+
 app.use(globalLimiter);
 app.use(cleanQuery);
 
 // اگر cors رو بالا گذاشتی، این هدرهای دستی معمولاً اضافه‌ان.
 // ولی اگر می‌خوای نگه داری، مشکلی نیست:
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // routes and middlewares
 app.use(express.json({ limit: "30mb" }));
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/public", (req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use("/public", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.static("public"));
 app.use("/api", router);
