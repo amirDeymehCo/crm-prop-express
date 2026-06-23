1c959c5b548be4600b8aa60206231e6c9db44ef4
amir mohammad deymeh 2 <104575719+amirDeymehCo@users.noreply.github.com>
2026-06-23 10:23:27 +0330

    add public_data:/app/public in docker

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,10 +2,10 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, path.join(__dirname, "../public"));
+  destination: function (req, file, cb) {
+    cb(null, "public");
   },
-  filename(req, file, cb) {
+  filename: function (req, file, cb) {
     cb(
       null,
       file.fieldname + "-" + Date.now() + path.extname(file.originalname),
