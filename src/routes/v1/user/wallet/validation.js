872651dc0edadc35ce22b930aa54a93ff620e4e0
amir mohammad deymeh 2 <104575719+amirDeymehCo@users.noreply.github.com>
2026-09-13 12:58:48 +0330

    change validation network wallet address

--- src/routes/v1/user/wallet/validation.js
+++ src/routes/v1/user/wallet/validation.js
@@ -2,28 +2,28 @@ const { body } = require("express-validator");
 
 module.exports = new (class {
   depositIRR() {
-    return [
-      body("amount_usd")
-        .notEmpty().withMessage("مبلغ را وارد نمایید")
-
-
-    ];
+    return [body("amount_usd").notEmpty().withMessage("مبلغ را وارد نمایید")];
   }
   depositUSD() {
-    return [
-      body("amount_usd")
-        .notEmpty().withMessage("مبلغ را وارد نمایید")
-
-    ];
+    return [body("amount_usd").notEmpty().withMessage("مبلغ را وارد نمایید")];
   }
   widthdrawRequest() {
     return [
-      body("amount_usd")
-        .notEmpty().withMessage("مبلغ را وارد نمایید"),
+      body("amount_usd").notEmpty().withMessage("مبلغ را وارد نمایید"),
+
       body("wallet_address")
-        .notEmpty().withMessage("آدرس کیف پول را وارد نمایید")
-        .matches(/^T[1-9A-HJ-NP-Za-km-z]{33}$/)
-        .withMessage("آدرس ولت معتبر نمی‌باشد (TRC20 USDT)."),
+        .notEmpty()
+        .withMessage("آدرس کیف پول را وارد نمایید")
+        .custom((value) => {
+          const isTRC20 = /^T[1-9A-HJ-NP-Za-km-z]{33}$/.test(value);
+          const isBEP20 = /^0x[a-fA-F0-9]{40}$/.test(value);
+
+          if (!isTRC20 && !isBEP20) {
+            throw new Error("آدرس ولت معتبر نمی‌باشد (TRC20 یا BEP20 USDT).");
+          }
+
+          return true;
+        }),
     ];
   }
 })();
